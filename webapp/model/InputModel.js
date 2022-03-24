3e4eb358bc4154e8ed34435828cacb6a7cb68fbc
Sittisak Chayowan <schayowan@deloitte.com>
2022-03-24 09:19:43 +0000

    Fix Count Substring from Barcode wrong

--- webapp/model/InputModel.js
+++ webapp/model/InputModel.js
@@ -232,7 +232,7 @@ sap.ui.define([
             var oInputData  =   this.getData();
 
             oInputData.Component            =   (sResult.length >= 22 ? sResult.substring(21,34) : sResult);
-            oInputData.Count                =   parseInt((sResult.length >= 58 ? sResult.substring(58,61) : sResult));
+            oInputData.Count                =   parseInt((sResult.length >= 58 ? sResult.substring(57,60) : sResult));
             oInputData.Barcode              =   sResult;
             this.setData(oInputData);
         },
